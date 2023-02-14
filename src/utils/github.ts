fb68ec264b81a8f465a93902f29e4b1d83e179f8
Mauricio Dottavio <dottavio@gmail.com>
2023-02-14 11:19:18 -0300

    fix: commit comparation

--- src/utils/github.ts
+++ src/utils/github.ts
@@ -1,15 +1,18 @@
 import type { Commits, OctokitClient, Repository } from "../types"
 import { getPrDescription, getPrNumber, getPrType } from "./commons"
 
-
+/**
+ * @todo Accept a list of branches to compare via config
+ */
 const getCommits = async (
   octokitClient: OctokitClient,
-  repo: Repository
+  repo: Repository,
+  compareBranches = { base: "main", head: "development" }
 ): Promise<Commits> => {
   const commitsRequest =
     await octokitClient.rest.repos.compareCommitsWithBasehead({
       ...repo,
-      basehead: "staging...development",
+      basehead: `${compareBranches.base}...${compareBranches.head}`,
     });
 
   const { commits: rawCommits } = commitsRequest.data;
